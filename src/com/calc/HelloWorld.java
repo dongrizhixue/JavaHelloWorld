c51de3d6d341bd98bb4b4294bf066a40c93e8951
dongrizhixue <853523391@qq.com>
2017-06-14 18:05:27 +0800

    update java

--- src/com/calc/HelloWorld.java
+++ src/com/calc/HelloWorld.java
@@ -3,27 +3,20 @@ package com.calc;
 import java.util.Arrays;
 
 public class HelloWorld {
-
 	public static void main(String[] args) {
 
-		// 创建名为hello的对象
+		// 创建对象，对象名为hello
 		HelloWorld hello = new HelloWorld();
 
-		// 调用hello对象的calcAvg()方法，并将返回值保存在变量avg中
-		double avg = hello.calcAvg();
-
-		System.out.println("平均成绩为：" + avg);
+		// 调用方法，传入两门课程的成绩
+		hello.calcAvg(94, 81);
 	}
 
-	// 定义一个返回值为double类型的方法
-	public double calcAvg() {
-
-		double java = 92.5;
-		double php = 83.0;
-		double avg = (java + php) / 2; // 计算平均值
-
-		// 使用return返回值
-		return avg;
-
+	/*
+	 * 功能：计算两门课程考试成绩的平均分并输出平均分 定义一个包含两个参数的方法，用来传入两门课程的成绩
+	 */
+	public void calcAvg(float a, float b) {
+		float avg = (a + b) / 2;
+		System.out.println("平均分：" + avg);
 	}
 }
