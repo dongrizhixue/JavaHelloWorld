b9aaf89ba0a47114d673c50367488e71240e9c74
dongrizhixue <853523391@qq.com>
2017-06-15 14:35:33 +0800

    7-8练习

--- src/com/calc/HelloWorld.java
+++ src/com/calc/HelloWorld.java
@@ -1,27 +1,28 @@
 package com.calc;
 
-import java.util.Arrays;
-
 public class HelloWorld {
 	public static void main(String[] args) {
+
+		// 创建对象
 		HelloWorld hello = new HelloWorld();
-		int[] scores = { 79, 52, 98, 81 };
 
-		// 调用方法，传入成绩数组，并获取成绩的个数
-		int count = hello.sort(scores);
-
-		System.out.println("共有" + count + "个成绩信息！");
+		// 调用无参的方法
+		hello.print();
+		// 调用带有一个字符串参数的方法
+		hello.print("imooc");
+		// 调用带有一个整型参数的方法
+		hello.print(18);
 	}
 
-	/*
-	 * 功能：将考试成绩排序并输出，返回成绩的个数 定义一个包含整型数组参数的方法，传入成绩数组 使用Arrays类对成绩数组进行排序并输出
-	 * 方法执行后返回数组中元素的个数
-	 */
-	public int sort(int[] scores) {
-		Arrays.sort(scores);
-		System.out.println(Arrays.toString(scores));
+	public void print() {
+		System.out.println("无参的print方法");
+	}
 
-		// 返回数组中元素的个数
-		return scores.length;
+	public void print(String name) {
+		System.out.println("带有一个字符串参数的print方法，参数值为：" + name);
+	}
+
+	public void print(int age) {
+		System.out.println("带有一个整型参数的print方法，参数值为：" + age);
 	}
 }
